dd4219275f48498f7285ab73d8edf2bd4d69f869
Ashwani Kumar <130587125+ashwanimaurya85@users.noreply.github.com>
2023-10-26 09:10:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
  
 function hasKey(key) {
@@ -14,4 +12,4 @@ function hasKey(key) {
  
 // Do not change the code below
 const key = prompt("Enter Key.");
-alert(hasKey(key)
+alert(hasKey(key));
